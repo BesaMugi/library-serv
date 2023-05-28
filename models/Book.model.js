7e0704b41323ee177477a200505067cceadfa34d
BesaMugi <mugiwarabesa@mail.ru>
2023-05-28 15:58:47 +0300

    gg

--- models/Book.model.js
+++ models/Book.model.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 
 const BookSchema = mongoose.Schema({
     book: String,
-        authorId: {
-            type: mongoose.SchemaTypes.ObjectId,
-            ref: "Author",
-        },
-        genreId: {
-            type: mongoose.SchemaTypes.ObjectId,
-            ref: "Genre"
-        }
+    authorId: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: "Author",
+    },
+    genreId: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: "Genre"
+    }
 });
   
 const Book = mongoose.model("Book", BookSchema);
